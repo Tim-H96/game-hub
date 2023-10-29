7f3ca13deefa7487c6696b7c66df5b833c140fad
Tim Hancock <82804714+Tim-H96@users.noreply.github.com>
2023-10-29 13:10:27 -0700

    Create Game Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    <>
-      <GameGrid />
-    </>
+    <Grid
+      templateAreas={`"nav nav"
+                        "left-pane main"`}
+      gridTemplateRows={"100px 1fr"}
+      gridTemplateColumns={"200px 1fr"}
+      gap="2"
+      bg={"gray.800"}
+    >
+      <GridItem pl="2" area={"nav"} bg={"orange.300"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" area={"left-pane"} bg={"blue.200"}>
+        Left Pane
+      </GridItem>
+      <GridItem pl="2" area={"main"} color={"white"}>
+        Game Grid
+        <GameGrid />
+      </GridItem>
+    </Grid>
   );
 }
 
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,43 +1,65 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  Grid,
+  GridItem,
+  Button,
+  AspectRatio,
+} from "@chakra-ui/react";
 
 interface Game {
   id: number;
-  metacritic: number;
+  added: number;
+  background_image: string;
   name: string;
-  playtime: number;
-  rating: number;
-  genres: [{ name: string }];
-  image: string;
-  released: string;
 }
 
 const GameGrid = () => {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
+
     apiClient
-      .get("/games")
+      .get("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
         console.log(res.data);
       })
       .catch((err) => {
         setError(err.message);
-        console.log(err);
       });
+    //return controller.abort();
   }, []);
 
   return (
     <>
       {error && <p>{error}</p>}
-      <ul>
-        Game Grid
-        {games.map((game: Game) =>
-          game.genres.map((g) => g.name === "RPG" && <li>{game.name}</li>)
-        )}
-      </ul>
+      <Grid templateColumns="repeat(3, 1fr)" minWidth={"200px"} gap={3}>
+        {games.map((game) => (
+          <GridItem key={game.id} minWidth={"200px"}>
+            <Card size={"md"} bg={"gray.700"}>
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src={game.background_image}
+                  objectFit={"cover"}
+                  borderTopRadius={"lg"}
+                ></Image>
+              </AspectRatio>
+
+              <CardHeader color={"white"}>
+                {game.name} <Button size={"xsm"}>{game.added}</Button>
+              </CardHeader>
+              <CardBody color={"white"}>Body</CardBody>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
     </>
   );
 };
