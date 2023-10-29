9663de01a8d4c5913e71bf6313e1d51b217ea3a9
Tim Hancock <82804714+Tim-H96@users.noreply.github.com>
2023-10-29 16:51:13 -0700

    Add Left Pane and Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import GameGrid from "./components/GameGrid";
 import useGames from "./hooks/useGames";
+import LeftPane from "./components/LeftPane";
 
 function App() {
   const { games, error } = useGames();
 
   return (
     <Grid
-      templateAreas={`"nav nav"
-                        "left-pane main"`}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "left-pane main"`,
+      }}
       gridTemplateRows={"100px 1fr"}
       gridTemplateColumns={"200px 1fr"}
       gap="2"
-      bg={"gray.800"}
     >
-      <GridItem pl="2" area={"nav"} bg={"orange.300"}>
+      <GridItem area={"nav"} bg={"orange.300"}>
         Nav
       </GridItem>
-      <GridItem pl="2" area={"left-pane"} bg={"blue.200"}>
-        Left Pane
-      </GridItem>
-      <GridItem pl="2" area={"main"} color={"white"}>
+      <Show above="lg">
+        <GridItem area={"left-pane"} bg={"blue.200"}>
+          <LeftPane onClick={() => console.log("clicked")} />
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} color={"white"} bg={"coral"}>
         Game Grid
         <GameGrid games={games} error={error} />
       </GridItem>
